de8bc2c430d729c6837464c31432793928ae9060
unknown <marlon_tancredo@hotmail.com>
2023-08-30 17:14:34 +0100

    refactor: refactoring App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import CatCard from "./components/cat_card";
+import { cats } from "./data/cat-data";
+
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App(): JSX.Element {
+  const catData = cats;
+  catData.forEach((cat) => (cat.id = uuidv4()));
 
-	// JavaScript/TypeScript code can be inserted here!
+  const [catsState, setCatsState] = useState(catData);
 
-	return (
-		<>
-			<Navbar />
-			<Header />
+  return (
+    <>
+      <Navbar />
+      <Header />
 
-			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
-			</main>
+      <main>
+        <div className="cards__wrapper">
+          {catsState.map((cat, index) => (
+            <CatCard
+              key={cat.id}
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+              catIndex={index}
+            />
+          ))}
+        </div>
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default App;
