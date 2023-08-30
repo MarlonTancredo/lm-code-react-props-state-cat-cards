5c5345aff2463daee8a701df4d0b711bb5ad46a2
unknown <marlon_tancredo@hotmail.com>
2023-08-30 17:10:09 +0100

    feat: implementing cat_card.tsx component

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,35 @@
+import CatImage from "./cat_image";
+import { images } from "../data/cat-images-data";
+
+interface CatProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+  catIndex: number;
+}
+
+const CatCard = ({
+  name,
+  species,
+  favFoods,
+  birthYear,
+  catIndex,
+}: CatProps) => (
+  <div className="card">
+    <h3 className="card__text card__header">{name}</h3>
+    <p className="card__text">Species: {species}</p>
+    <p className="card__text">Favourite Food(s): {favFoods}</p>
+    <p className="card__text">Birth Year: {birthYear}</p>
+    <CatImage
+      image={images[catIndex].image}
+      altText={images[catIndex].altText}
+      licenseType={images[catIndex].licenseType}
+      licenseUrl={images[catIndex].licenseUrl}
+      attributionName={images[catIndex].attributionName}
+      attributionUrl={images[catIndex].attributionUrl}
+    />
+  </div>
+);
+
+export default CatCard;
