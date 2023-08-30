e8c26f6db78ebf83d39f3af295094ba329db5cb0
unknown <marlon_tancredo@hotmail.com>
2023-08-30 18:18:26 +0100

    feat: importing dogData into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import CatCard from "./components/cat_card";
+import PetCard from "./components/pet_card";
 import { catData } from "./data/cat-data";
+// import { dogData } from "./data/dog-data"; //An dog-images-data.ts must be created.
 
 function App(): JSX.Element {
   return (
@@ -13,7 +14,7 @@ function App(): JSX.Element {
       <main>
         <div className="cards__wrapper">
           {catData.map((cat, index) => (
-            <CatCard
+            <PetCard
               key={cat.id}
               name={cat.name}
               species={cat.species}
@@ -23,6 +24,19 @@ function App(): JSX.Element {
             />
           ))}
         </div>
+        {/* Uncomment */}
+        {/* <div className="cards__wrapper">
+          {dogData.map((dog, index) => (
+            <CatCard
+              key={dog.id}
+              name={dog.name}
+              species={dog.species}
+              favFoods={dog.favFoods}
+              birthYear={dog.birthYear}
+              catIndex={index}
+            />
+          ))}
+        </div> */}
       </main>
       <Footer />
     </>
