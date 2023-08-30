556349d752f293f6a35f942783ce79d66c4d9297
unknown <marlon_tancredo@hotmail.com>
2023-08-30 17:13:50 +0100

    feat: implementing Cat interface

--- src/data/cat.ts
+++ src/data/cat.ts
@@ -1,1 +1,8 @@
-export default interface Cat {}
+export default interface Cat {
+  id?: string;
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+  catIndex?: number;
+}
